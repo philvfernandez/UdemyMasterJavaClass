a9456c4009b764d54e6b355b03a28c74ad9b211b
Phil Fernandez <philvfernandez@gmail.com>
2023-02-07 14:50:04 -0700

    More Lambda expression examples.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.philf.CodeToRun;
 import com.philf.Employee;
+import com.philf.UpperConcat;
 
 import java.util.*;
 
@@ -24,11 +25,11 @@ public class Main {
         // Note: () -> System.out.println("Printing from Lambda") is an empty argument list.
         // Note: the --> is the arrow token.
         // Note: The body is the code we want to run. Which in the eample below is System.out.println("Printing from Lambda")
-        new Thread(() -> {
-            System.out.println("Printing from Runnable");
-            System.out.println("Line 2");
-            System.out.format("This is line %d\n",3);
-        }).start();
+//        new Thread(() -> {
+//            System.out.println("Printing from Runnable");
+//            System.out.println("Line 2");
+//            System.out.format("This is line %d\n",3);
+//        }).start();
 
         Employee john = new Employee("John Doe", 30);
         Employee tim = new Employee("Phil Fernandez", 49);
@@ -41,17 +42,44 @@ public class Main {
         employeeList.add(jack);
         employeeList.add(snow);
 
-        Collections.sort(employeeList, new Comparator<Employee>() {
-            @Override
-            public int compare(Employee employee1, Employee employee2) {
-                return employee1.getName().compareTo(employee2.getName());
-            }
-        });
+//        Collections.sort(employeeList, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
+//
+//        for(Employee employee : employeeList) {
+//            System.out.println(employee.getName());
+//        }
 
+        //Above sort example using a Lambda expression
+        Collections.sort(employeeList, (employee1, employee2) -> {
+            return employee1.getName().compareTo(employee2.getName());
+        });
         for(Employee employee : employeeList) {
             System.out.println(employee.getName());
         }
 
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//        employeeList.get(0).getName(), employeeList.get(2).getName());
+//        System.out.println(sillyString);
+
+        //Using Lambdas
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillString = doStringStuff(uc, employeeList.get(0).getName(), employeeList.get(1).getName());
+        System.out.println(sillString);
+
+    }
+
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1,s2);
 
     }
 }
---
+++ src/com/philf/UpperConcat.java
@@ -0,0 +1,5 @@
+package com.philf;
+
+public interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
+}
