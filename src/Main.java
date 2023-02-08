4b2d5e81721c1f0d153f1677af064f278a1ce78d
Phil Fernandez <philvfernandez@gmail.com>
2023-02-08 15:01:26 -0700

    Added example that uses functional interfaces and predicates.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.philf.Employee;
 import com.philf.UpperConcat;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -12,91 +13,31 @@ public class Main {
         Employee tim = new Employee("Phil Fernandez", 49);
         Employee jack = new Employee("Jack Hill", 40);
         Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red RidingHood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
 
         List<Employee> employeeList = new ArrayList<>();
         employeeList.add(john);
         employeeList.add(tim);
         employeeList.add(jack);
         employeeList.add(snow);
-
-        employeeList.forEach(employee -> {
-            System.out.println(employee.getName());
-            System.out.println(employee.getAge());
-        });
-
-//        for(Employee employee: employeeList) {
-//            System.out.println(employee.getName());
-//            new Thread(() -> System.out.println(employee.getAge())).start();
-//        }
+        employeeList.add(red);
+        employeeList.add(charming);
 
 
-
-
+        printEmployeesByAge(employeeList, "Employees Over 30", employee -> employee.getAge() > 30);
+        printEmployeesByAge(employeeList, "Employees 30 and Under", employee -> employee.getAge() < 30);
 
     }
 
-
-    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
-        return uc.upperAndConcat(s1,s2);
-
-    }
-
-}
-
-//Anonymous class.
-class AnotherClass {
-    public String doSomething() {
-        int i = 0; //This value can't as it's final.
-
-        UpperConcat uc = (s1, s2) -> {
-            System.out.println("The Lambda expression's class is: " + getClass().getSimpleName());
-            System.out.println("i in the lambda expression = " + i);
-            String result = s1.toUpperCase() + s2.toUpperCase();
-            return result;
-
-        };
-
-//        int j = 0;
-//        {
-//            String s1;
-//            String s2;
-//            System.out.println("The Lambda expression's class is: " + getClass().getSimpleName());
-//            System.out.println("i in the lambda expression = " + i);
-//
-//            //S1 and S2 are not available outside of the lambda.
-//            String result = s1.toUpperCase() + s2.toUpperCase();
-//            return result;
-//
-//
-//        }
-
-        {
-            System.out.println("The AnotherClass class's name is " + getClass().getSimpleName());
-            return Main.doStringStuff(uc, "String1", "String2");
-        }
-
-    }
-//        System.out.println("The AnotherClass class's is: " + getClass().getSimpleName());
-//        return Main.doStringStuff(new UpperConcat() {
-//            @Override
-//            public String upperAndConcat(String s1, String s2) {
-//                System.out.println("The anonymous class's name is: " + getClass().getSimpleName());
-//                return s1.toUpperCase() + s2.toUpperCase();
-//            }
-//        }, "String1", "String2");
-//    }
-
-    public void printValue() {
-        int number = 25;
-        Runnable r = () -> {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    private static void printEmployeesByAge(List<Employee> employees,
+                                            String ageText, Predicate<Employee> ageCondition) {
+        System.out.println(ageText);
+        System.out.println("====================");
+        for (Employee employee : employees) {
+            if(ageCondition.test(employee)) {
+                System.out.println(employee.getName());
             }
-            System.out.println("The value is " + number);
-        };
-
-        new Thread(r).start();
+        }
     }
 }
