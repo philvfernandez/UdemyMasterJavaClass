76d51575e5d9deb23b935059ddc266dd1e701e42
Phil Fernandez <philvfernandez@gmail.com>
2023-02-08 10:24:01 -0700

    Lambda expressions scope and functional programming.

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //new Thread(new CodeToRun()).start();
-
-        //second way to create a new Thread - using an anonymous class
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("printing from the Runnable");
-//            }
-//        }).start();
-
-        //using lambdas -- Note: Lambdas are a feature in Java release 8+
-        //Lambda expressions have 3 parts:
-        // 1. Argument list
-        // 2. The arrow token
-        // 3. The body.
-        // Note: () -> System.out.println("Printing from Lambda") is an empty argument list.
-        // Note: the --> is the arrow token.
-        // Note: The body is the code we want to run. Which in the eample below is System.out.println("Printing from Lambda")
-//        new Thread(() -> {
-//            System.out.println("Printing from Runnable");
-//            System.out.println("Line 2");
-//            System.out.format("This is line %d\n",3);
-//        }).start();
 
         Employee john = new Employee("John Doe", 30);
         Employee tim = new Employee("Phil Fernandez", 49);
@@ -42,46 +19,16 @@ public class Main {
         employeeList.add(jack);
         employeeList.add(snow);
 
-//        Collections.sort(employeeList, new Comparator<Employee>() {
-//            @Override
-//            public int compare(Employee employee1, Employee employee2) {
-//                return employee1.getName().compareTo(employee2.getName());
-//            }
-//        });
-//
-//        for(Employee employee : employeeList) {
+        employeeList.forEach(employee -> {
+            System.out.println(employee.getName());
+            System.out.println(employee.getAge());
+        });
+
+//        for(Employee employee: employeeList) {
 //            System.out.println(employee.getName());
+//            new Thread(() -> System.out.println(employee.getAge())).start();
 //        }
 
-        //Above sort example using a Lambda expression
-        Collections.sort(employeeList, (employee1, employee2) -> {
-            return employee1.getName().compareTo(employee2.getName());
-        });
-        for(Employee employee : employeeList) {
-            System.out.println(employee.getName());
-        }
-
-//        String sillyString = doStringStuff(new UpperConcat() {
-//            @Override
-//            public String upperAndConcat(String s1, String s2) {
-//                return s1.toUpperCase() + s2.toUpperCase();
-//            }
-//        },
-//        employeeList.get(0).getName(), employeeList.get(2).getName());
-//        System.out.println(sillyString);
-
-        //Using Lambdas
-//        UpperConcat uc = (s1, s2) -> {
-//            String result = s1.toUpperCase() + s2.toUpperCase();
-//            return result;
-//        };
-//
-//        String sillString = doStringStuff(uc, employeeList.get(0).getName(), employeeList.get(1).getName());
-//        System.out.println(sillString);
-
-        AnotherClass anotherClass = new AnotherClass();
-        String s = anotherClass.doSomething();
-        System.out.println(s);
 
 
 
@@ -99,12 +46,29 @@ public class Main {
 //Anonymous class.
 class AnotherClass {
     public String doSomething() {
-        int i = 0;
+        int i = 0; //This value can't as it's final.
+
         UpperConcat uc = (s1, s2) -> {
             System.out.println("The Lambda expression's class is: " + getClass().getSimpleName());
+            System.out.println("i in the lambda expression = " + i);
             String result = s1.toUpperCase() + s2.toUpperCase();
             return result;
+
         };
+
+//        int j = 0;
+//        {
+//            String s1;
+//            String s2;
+//            System.out.println("The Lambda expression's class is: " + getClass().getSimpleName());
+//            System.out.println("i in the lambda expression = " + i);
+//
+//            //S1 and S2 are not available outside of the lambda.
+//            String result = s1.toUpperCase() + s2.toUpperCase();
+//            return result;
+//
+//
+//        }
 
         {
             System.out.println("The AnotherClass class's name is " + getClass().getSimpleName());
@@ -121,4 +85,18 @@ class AnotherClass {
 //            }
 //        }, "String1", "String2");
 //    }
+
+    public void printValue() {
+        int number = 25;
+        Runnable r = () -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("The value is " + number);
+        };
+
+        new Thread(r).start();
+    }
 }
