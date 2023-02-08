5a4a538db961586e4177f2a68447eb4f7f97b4bd
Phil Fernandez <philvfernandez@gmail.com>
2023-02-08 15:35:19 -0700

    Examples using functions.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.philf.Employee;
 import com.philf.UpperConcat;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -27,31 +28,60 @@ public class Main {
         employeeList.add(charming);
 
 
-        printEmployeesByAge(employeeList, "Employees Over 30", employee -> employee.getAge() > 30);
-        printEmployeesByAge(employeeList, "Employees 30 and Under", employee -> employee.getAge() < 30);
-        printEmployeesByAge(employeeList, "Employees younger than 25", new Predicate<Employee>() {
-            @Override
-            public boolean test(Employee employee) {
-                return employee.getAge() < 25;
+//        printEmployeesByAge(employeeList, "Employees Over 30", employee -> employee.getAge() > 30);
+//        printEmployeesByAge(employeeList, "Employees 30 and Under", employee -> employee.getAge() < 30);
+//        printEmployeesByAge(employeeList, "Employees younger than 25", new Predicate<Employee>() {
+//            @Override
+//            public boolean test(Employee employee) {
+//                return employee.getAge() < 25;
+//
+//            }
+//        });
+//        IntPredicate greaterThan15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//
+//        System.out.println(greaterThan15.test(10));
+//        int a = 20;
+//        System.out.println(greaterThan15.test(a + 5));
+//        System.out.println(greaterThan15.and(lessThan100).test(50));
+//        System.out.println(greaterThan15.and(lessThan100).test(15));
+//
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+//
+//        for(int i=0;i<10;i++) {
+//            System.out.println(randomSupplier.get());
+//        }
+//
+//        employeeList.forEach(employee -> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(' ') + 1);
+//            System.out.println("Last Name is: " + lastName);
+//        });
 
+        Function<Employee, String> getLastName = (Employee employee) -> {
+            return employee.getName().substring(employee.getName().indexOf(' ') + 1);
+        };
+
+        String lastName = getLastName.apply(employeeList.get(1));
+        System.out.println(lastName);
+
+        Function<Employee, String> getFirstName = (Employee employee) -> {
+            return employee.getName().substring(0, employee.getName().indexOf(' '));
+        };
+
+        Random random1 = new Random();
+        for(Employee employee : employeeList) {
+            if(random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
             }
-        });
-        IntPredicate greaterThan15 = i -> i > 15;
-        IntPredicate lessThan100 = i -> i < 100;
-
-        System.out.println(greaterThan15.test(10));
-        int a = 20;
-        System.out.println(greaterThan15.test(a + 5));
-        System.out.println(greaterThan15.and(lessThan100).test(50));
-        System.out.println(greaterThan15.and(lessThan100).test(15));
-
-        Random random = new Random();
-        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
-
-        for(int i=0;i<10;i++) {
-            System.out.println(randomSupplier.get());
         }
 
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees,
