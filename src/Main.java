ffa1e937da92d7e23082f0ce06b7bf4fa02dc15c
Phil Fernandez <philvfernandez@gmail.com>
2023-02-08 15:21:29 -0700

    Added another example that uses functional interfaces and predicates.

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import com.philf.Employee;
 import com.philf.UpperConcat;
 
 import java.util.*;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Employee john = new Employee("John Doe", 30);
-        Employee tim = new Employee("Phil Fernandez", 49);
+        Employee tim = new Employee("Phil Fernandez", 19);
         Employee jack = new Employee("Jack Hill", 40);
         Employee snow = new Employee("Snow White", 22);
         Employee red = new Employee("Red RidingHood", 35);
@@ -27,6 +29,28 @@ public class Main {
 
         printEmployeesByAge(employeeList, "Employees Over 30", employee -> employee.getAge() > 30);
         printEmployeesByAge(employeeList, "Employees 30 and Under", employee -> employee.getAge() < 30);
+        printEmployeesByAge(employeeList, "Employees younger than 25", new Predicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getAge() < 25;
+
+            }
+        });
+        IntPredicate greaterThan15 = i -> i > 15;
+        IntPredicate lessThan100 = i -> i < 100;
+
+        System.out.println(greaterThan15.test(10));
+        int a = 20;
+        System.out.println(greaterThan15.test(a + 5));
+        System.out.println(greaterThan15.and(lessThan100).test(50));
+        System.out.println(greaterThan15.and(lessThan100).test(15));
+
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+
+        for(int i=0;i<10;i++) {
+            System.out.println(randomSupplier.get());
+        }
 
     }
 
